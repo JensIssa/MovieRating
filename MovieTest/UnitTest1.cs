a5d3f37b3332281268bfde912fe082d21bde40aa
AndreasBerthelsen <andr59j9@easv365.dk>
2022-09-28 10:37:37 +0200

    test 2

--- MovieTest/UnitTest1.cs
+++ MovieTest/UnitTest1.cs
@@ -31,6 +31,34 @@ public class UnitTest1
         Assert.Equal(expectedResult,result);
         mockRepo.Verify(r => r.getAllReviews(), Times.Once);
     }
+
+    [Theory]
+    [InlineData(1,2)]
+    [InlineData(1,4)]//true
+    [InlineData(2,3)]
+    [InlineData(2,4)]//true
+    public void GetAverageRateFromReviewerTest(int reviewerId, double expectedResult)
+    {
+        //Arrange
+        List<BEReview> fakeRepo = new List<BEReview>
+        {
+            new BEReview { Reviewer = 1, Movie = 1, Grade = 3, ReviewDate = DateTime.Now },
+            new BEReview { Reviewer = 1, Movie = 2, Grade = 5, ReviewDate = DateTime.Now },
+            new BEReview { Reviewer = 2, Movie = 1, Grade = 4, ReviewDate = DateTime.Now },
+            new BEReview { Reviewer = 2, Movie = 2, Grade = 4, ReviewDate = DateTime.Now }
+        };
+        Mock<IBERepository> mockRepo = new Mock<IBERepository>();
+        mockRepo.Setup(r => r.getAllReviews()).Returns(fakeRepo);
+
+        IService service = new MovieService(mockRepo.Object);
+        
+        //Act
+        var result = service.GetAverageRateFromReviewer(reviewerId);
+        
+        //Assert
+        Assert.Equal(expectedResult, result);
+        mockRepo.Verify(r=>r.getAllReviews(), Times.Once);
+    }
     
     
 }
