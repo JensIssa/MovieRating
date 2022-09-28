e1fe5c86271368d9f8a135f0c745a3211b5322e9
Jens Bassem Hanna Issa <jens7391@easv365.dk>
2022-09-28 10:45:28 +0200

    send it boi

--- MovieTest/UnitTest1.cs
+++ MovieTest/UnitTest1.cs
@@ -59,6 +59,33 @@ public class UnitTest1
         Assert.Equal(expectedResult, result);
         mockRepo.Verify(r=>r.getAllReviews(), Times.Once);
     }
-    
+
+    [Theory]
+    [InlineData(1, 5, 2)] // false
+    [InlineData(1, 3, 2)] //true
+    [InlineData(2, 3, 1)] //false
+    [InlineData(2, 4, 2)] //true
+    public void GetNumberOfRatesTest(int reviewerID, int rate, int expectedRate)
+    {
+        //Arrange
+        List<BEReview> fakeRepo = new List<BEReview>
+        {
+            new BEReview { Reviewer = 1, Movie = 1, Grade = 3, ReviewDate = DateTime.Now },
+            new BEReview { Reviewer = 1, Movie = 2, Grade = 3, ReviewDate = DateTime.Now },
+            new BEReview { Reviewer = 2, Movie = 1, Grade = 4, ReviewDate = DateTime.Now },
+            new BEReview { Reviewer = 2, Movie = 2, Grade = 4, ReviewDate = DateTime.Now }
+        };
+        Mock<IBERepository> mockRepo = new Mock<IBERepository>();
+        mockRepo.Setup(r => r.getAllReviews()).Returns(fakeRepo);
+
+        IService service = new MovieService(mockRepo.Object);
+        
+        //Act
+        var result = service.GetNumberOfRatesByReviewer(reviewerID, rate);
+        
+        //Assert
+        Assert.Equal(expectedRate, result);
+        mockRepo.Verify(r => r.getAllReviews(), Times.Once);
+    }
     
 }
